a983c1dbd04a47ada77980c885b3ae21967d2044
oreols <oreak1500@gmail.com>
2023-10-11 13:22:09 +0200

    + dodana klasa node i klasa listydwukierunkowej (niedokonczona) - do zrobienia metody w klasie listydwukierunkowej i reszta funkcjonalnosci

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+ 
+#include <iostream>
+using namespace std;
+ 
+
+
+class Node {
+    public:
+        int data;
+        Node* next;
+        Node* prev;
+        Node(int data): data(data), next(NULL), prev(NULL){}
+        
+
+};
+
+class DoublyLinkedList{
+    private:
+        Node* head;
+        Node* tail;
+
+
+};
+
+
+int main() {
+    return 0;
+}
